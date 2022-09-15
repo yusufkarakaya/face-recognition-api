9a596f6ec7aa5c5f549349b9284874c8dfee26e2
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-09-15 15:09:59 -0700

    fix

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
